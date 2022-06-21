b702248d0c03b4fa8e4373f01e1f97653941d585
Jared Forsyth <jared@jaredforsyth.com>
2022-06-20 22:03:45 -0500

    cleanup

--- core/expressionWrappers/Unary.ts
+++
@@ -1,9 +0,0 @@
-// This is just sugar for an apply
-
-/*:peg:
-UnaryOp = op_drop:unaryOpWithHash? inner:UnaryInner 
-unaryOpWithHash = op:unaryOp hash:(($opHash / $BuiltinHash) _)?
-unaryOp = "-" / "!"
-
-UnaryInner = Apply
-*/
