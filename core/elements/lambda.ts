bdef7497bb0ae933cfcfa9916cc0fb062aa2e496
Jared Forsyth <jared@jaredforsyth.com>
2022-07-29 14:59:03 -0500

    ok tests working agai

--- core/elements/lambda.ts
+++ core/elements/lambda.ts
@@ -242,7 +242,7 @@ export const Analyze: Visitor<{ ctx: ACtx; hit: {} }> = {
         let changed = false;
         const args = node.args.map((arg) => {
             getLocals(arg.pat, arg.typ, locals, ctx.ctx);
-            const t = ctx.ctx.resolveAnalyzeType(arg.typ);
+            const t = ctx.ctx.resolveAnalyzeType(arg.typ) ?? arg.typ;
             // console.log('check it', arg.pat, arg.typ);
             if (!t || !typeMatchesPattern(arg.pat, t, ctx.ctx)) {
                 // console.log('hup hit it');
