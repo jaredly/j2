dc65826028ca0f5701d424fe83063717004f9598
Jared Forsyth <jared@jaredforsyth.com>
2022-08-05 23:43:34 -0500

    hmmmm

--- devui/widgets/vdom.tsx
+++ devui/widgets/vdom.tsx
@@ -1,68 +1,84 @@
 import { noloc } from '../../core/consts';
 import { builtinContext, FullContext, GlobalUserType } from '../../core/ctx';
+import { processTypeFile } from '../../core/full/full';
 import { parseTypeFile } from '../../core/grammar/base.parser';
-import { Id, toId } from '../../core/ids';
+import { extract, Id, toId } from '../../core/ids';
 import * as t from '../../core/typed-ast';
 import { collapseConstraints } from '../../core/typing/analyze';
 import { applyType } from '../../core/typing/getType';
+import { maybeExpandTask } from '../../core/typing/tasks';
 import { ConstraintMap, typeMatches } from '../../core/typing/typeMatches';
 
-const loadTypes = (text: string) => {
+const loadTypes = (text: string, ctx: FullContext) => {
     const named: { [key: string]: Id } = {};
-    let ctx = builtinContext.clone();
 
-    parseTypeFile(text).toplevels.forEach((ast) => {
-        if (ast.type === 'TypeAlias') {
-            const top = ctx.ToTast.TypeAlias(ast, ctx);
-            const res = ctx.withTypes(top.elements);
-            ctx = res.ctx as FullContext;
-            top.elements.forEach((el, idx) => {
-                named[el.name] = toId(res.hash, idx);
+    const result = processTypeFile(text, ctx);
+    if (result.type === 'Error') {
+        throw new Error(`Failed to load`);
+    }
+    result.info.forEach((info) => {
+        if (info.contents.top.type === 'TypeAlias') {
+            const hash = info.contents.top.hash!;
+            info.contents.top.elements.forEach((el, i) => {
+                named[el.name] = toId(hash, i);
             });
         }
     });
-    return { ctx, named };
+
+    return { ctx: result.ctx, named };
 };
 
-const { ctx, named } = loadTypes(`
+const allTypes = `
 type attrs = <Effects: task>{
     style: {display: string = "", flex: uint = 1u, padding: string = ""} = (),
     onClick: () => Task<Effects, ()> = () => \`Return(),
 }
 type Node = <Effects: task>[\`Node(string, attrs<Effects>, Array<Node<Effects>>) | \`Text(string)]
 type Render = <State>Task<[\`Render((state: State) => Node<[\`SetState(State, ())]>, [])], ()>
-`);
+`;
 
-const type = ctx.typeForId(named.Render) as GlobalUserType;
+export const vdomWidget = (type: t.Type, value: any, baseCtx: FullContext) => {
+    const types = loadTypes(allTypes, baseCtx);
+    const ctx = types.ctx;
 
-export const vdomWidget = (type: t.Type, value: any) => {
     const constraints: ConstraintMap = {};
-    debugger;
     const tv = ctx.newTypeVar(noloc);
+
+    if (!types.named.Render) {
+        return console.error('no Render');
+    }
+    const { hash } = extract(types.named.Render);
+    if (!ctx.extract().types.hashed[hash]) {
+        return console.error('no Render in ctx');
+    }
+
     const Render = ctx.resolveAnalyzeType({
         type: 'TRef',
-        ref: { type: 'Global', id: named.Render },
+        ref: { type: 'Global', id: types.named.Render },
         loc: noloc,
     });
+    if (!Render) {
+        console.error('not resolved');
+        return;
+    }
     const applied = applyType([tv], Render as t.TVars, ctx);
     if (!applied) {
         return;
     }
-    // const expected = ctx.resolveRefsAndApplies({
-    //     type: 'TApply',
-    //     target: Render,
-    //     args: [tv],
-    //     loc: noloc,
-    // });
-    // if (!expected) {
-    //     console.log('No good resolution');
-    //     return;
-    // }
-    if (!typeMatches(type, applied, ctx, [], constraints)) {
-        console.log('didnt match either');
-        return;
-    }
-    console.log(collapseConstraints(constraints[tv.id] || {}, ctx));
-};
 
-// Render :
+    const tt = maybeExpandTask(applied, ctx) ?? applied;
+    // const failures: Failures = [];
+    console.log('vdomwidget');
+    debugger;
+    // if (!typeMatches(type, tt, ctx, [], constraints, failures)) {
+
+    // Need to be able to typeMatches on two recursive types
+    const ok = false;
+    if (ok) {
+        if (!typeMatches(type, applied, ctx, [], constraints)) {
+            console.log('didnt match either');
+            return;
+        }
+        console.log('YES', collapseConstraints(constraints[tv.id] || {}, ctx));
+    }
+};
