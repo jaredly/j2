b9900a9de5c633e745bf853e3d2d378c85d370cf
Jared Forsyth <jared@jaredforsyth.com>
2022-10-07 09:06:44 -0500

    empty CallSuffix thing

--- nore/editor/manual/Empty.tsx
+++ nore/editor/manual/Empty.tsx
@@ -1,10 +1,12 @@
 import { Path } from '../generated/react-map';
 import React, { useLayoutEffect, useMemo, useRef } from 'react';
-import { setSelection, Store } from '../store/store';
+import { setSelection, Store, updateStore } from '../store/store';
 import { goLeft, goRight } from './navigation';
 import { handleKey, keyHandlers } from './keyHandlers';
 import * as parsers from '../../generated/parser';
 import { colors, getPos, pathColor } from './AtomEdit';
+import * as t from '../../generated/type-map';
+import * as to from '../../generated/to-map';
 
 export const Empty = ({
     path,
@@ -39,58 +41,99 @@ export const Empty = ({
             return null;
         }
     }, [edit]);
-    console.log('parsed', edit, parsed?.type);
-    if (sel) {
-        return (
-            <span
-                contentEditable
-                ref={ref}
-                style={{
-                    height: '1em',
-                    outline: 'none',
-                    color: (parsed && colors[parsed.type]) || 'white',
-                }}
-                onBlur={() => {
-                    setSelection(store, null);
-                }}
-                onInput={(evt) => setEdit(evt.currentTarget.textContent!)}
-                onKeyDown={(evt) => {
-                    if (
-                        evt.key === 'ArrowRight' &&
-                        getSelection()?.toString() === '' &&
-                        getPos(evt.currentTarget) ===
-                            evt.currentTarget.textContent!.length
-                    ) {
-                        evt.preventDefault();
-                        evt.stopPropagation();
-                        const right = goRight(store, path, true);
-                        if (right) {
-                            setSelection(store, right.sel);
-                        }
-                    }
-                    if (
-                        evt.key === 'ArrowLeft' &&
-                        getSelection()?.toString() === '' &&
-                        getPos(evt.currentTarget) === 0
-                    ) {
-                        evt.preventDefault();
-                        evt.stopPropagation();
-                        const left = goLeft(store, path, true);
-                        if (left) {
-                            setSelection(store, left.sel);
-                        }
-                    }
-                    if (keyHandlers[evt.key]) {
-                        const res = handleKey(store, path, evt.key, edit);
-                        if (res !== false) {
-                            evt.preventDefault();
-                            evt.stopPropagation();
-                            console.log(path);
-                        }
-                    }
-                }}
-            />
-        );
+
+    const commit = React.useCallback(() => {
+        if (!kind || !edit.length) {
+            return;
+        }
+        try {
+            // @ts-ignore
+            const parsed = parsers['parse' + kind](edit!.trim());
+            const update: t.Map = {};
+            // @ts-ignore
+            const tomap = to[kind](parsed, update);
+            update[parsed.loc.idx] = {
+                type: kind,
+                value: tomap,
+            } as t.Map[0];
+            // updateStore(store, { map: update });
+            const last = path[path.length - 1];
+            const parent = store.map[last.idx].value;
+            if (parent.type === 'CallSuffix' && last.cid === 0) {
+                updateStore(store, {
+                    map: {
+                        ...update,
+                        [last.idx]: {
+                            ...store.map[last.idx],
+                            value: { ...parent, args: [parsed.loc.idx] },
+                        } as t.Map[0],
+                    },
+                });
+            }
+        } catch (err) {
+            console.error(err);
+            setEdit('');
+            // setEdit(config.toString(value));
+        }
+    }, [edit, kind]);
+
+    useLayoutEffect(() => {
+        if (!sel) {
+            commit();
+        }
+    }, [sel, edit, kind]);
+
+    if (!sel) {
+        return <span style={{ height: '1em', color: 'red' }} />;
     }
-    return <span style={{ height: '1em', color: 'red' }}></span>;
+    return (
+        <span
+            contentEditable
+            ref={ref}
+            style={{
+                height: '1em',
+                outline: 'none',
+                color: (parsed && colors[parsed.type]) || 'white',
+            }}
+            onBlur={() => {
+                setSelection(store, null);
+            }}
+            onInput={(evt) => setEdit(evt.currentTarget.textContent!)}
+            onKeyDown={(evt) => {
+                if (
+                    evt.key === 'ArrowRight' &&
+                    getSelection()?.toString() === '' &&
+                    getPos(evt.currentTarget) ===
+                        evt.currentTarget.textContent!.length
+                ) {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                    const right = goRight(store, path, true);
+                    if (right) {
+                        setSelection(store, right.sel);
+                    }
+                }
+                if (
+                    evt.key === 'ArrowLeft' &&
+                    getSelection()?.toString() === '' &&
+                    getPos(evt.currentTarget) === 0
+                ) {
+                    evt.preventDefault();
+                    evt.stopPropagation();
+                    const left = goLeft(store, path, true);
+                    if (left) {
+                        setSelection(store, left.sel);
+                    }
+                }
+                if (keyHandlers[evt.key]) {
+                    const res = handleKey(store, path, evt.key, edit);
+                    if (res !== false) {
+                        evt.preventDefault();
+                        evt.stopPropagation();
+                        console.log(path);
+                    }
+                }
+            }}
+        />
+    );
 };
